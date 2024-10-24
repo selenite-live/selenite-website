c1e9c65d3384aaaf233162177367dd73e7791046
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2024-10-24 19:12:15 +0200

    fix(eslint): update tailwindanimate require to import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,9 @@
 import type { Config } from "tailwindcss";
+import animatePlugin from "tailwindcss-animate";
 
 const config: Config = {
   darkMode: ["class"],
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
@@ -68,6 +65,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animatePlugin],
 };
+
 export default config;
